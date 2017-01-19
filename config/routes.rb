b5087d49850671bf92598697ccc46d6c0e9706fd
Romain Diegoni <romain_diegoni@hotmail.com>
2017-01-19 09:56:40 +0000

    WIP: try to resolve action cable connection

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  mount ActionCable.server => '/cable'
-  
   resources :comments
 
   get '/api' => redirect('/swagger/dist/index.html?url=/apidocs/api-docs.json')
