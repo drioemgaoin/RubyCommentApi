07a594197975e9be197d1c5f56d6203fd986a498
Romain Diegoni <romain_diegoni@hotmail.com>
2017-01-19 09:45:38 +0000

    Add the mount for action cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
+  
   resources :comments
 
   get '/api' => redirect('/swagger/dist/index.html?url=/apidocs/api-docs.json')
