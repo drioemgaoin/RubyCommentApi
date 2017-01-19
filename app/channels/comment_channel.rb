0ce53ddf99e0fe7d74b994adb6807302e4d9a780
Romain Diegoni <romain_diegoni@hotmail.com>
2017-01-19 11:42:54 +0000

    WIP: try to resolve action cable connection

--- app/channels/comment_channel.rb
+++ app/channels/comment_channel.rb
@@ -1,11 +1,8 @@
   class CommentChannel < ApplicationCable::Channel
     def subscribed
-      puts "Subscribe to Comment Channel"
       stream_from "comment_channel"
     end
 
     def unubscribed
-      puts "Unsubscribe to Comment Channel"
-      stop_all_streams
     end
   end
