1898c78d296c56af4c58c8aeef08bc71ff4bc7e8
Romain Diegoni <romain_diegoni@hotmail.com>
2017-01-19 11:05:42 +0000

    Add logs to resolve heroku action cable connection

--- app/channels/comment_channel.rb
+++ app/channels/comment_channel.rb
@@ -1,9 +1,11 @@
   class CommentChannel < ApplicationCable::Channel
     def subscribed
+      puts "Subscribe to Comment Channel"
       stream_from "comment_channel"
     end
 
     def unubscribed
+      puts "Unsubscribe to Comment Channel"
       stop_all_streams
     end
   end
